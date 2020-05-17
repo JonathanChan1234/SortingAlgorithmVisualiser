89c068d3c324b1fbfa02cee37419a604c866dd56
JonathanChan1234 <jonathan19970428@gmail.com>
2020-05-17 16:33:45 +0800

    Updates

---
+++ precache-manifest.511d56366fff134e28929e7a9028779e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9f7f4c6a5e78e76ced504e99a92903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3d5d82ec63816cd3d11",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "2b35adb1bfe62ea8bcba",
+    "url": "/static/js/2.cf7e3637.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/static/js/2.cf7e3637.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3d5d82ec63816cd3d11",
+    "url": "/static/js/main.c1d3e6d3.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
